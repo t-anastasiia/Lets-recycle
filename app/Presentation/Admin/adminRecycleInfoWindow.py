5a4700918931241ea89ee442f25ccea7e5ce9d5d
nas.www.ay <nastaytalmazan@gmail.com>
2024-06-23 22:58:28 +1000

    recycle info update from admin

---
+++ app/Presentation/Admin/adminRecycleInfoWindow.py
@@ -0,0 +1,74 @@
+import os
+from PyQt6 import QtWidgets, QtCore
+from Presentation.UI.Admin.UiAdminRecycleInfoWindow import Ui_MainWindow
+import sqlite3
+
+
+class AdminRecycleInfoWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        self.backButton.clicked.connect(self.on_back_button_clicked)
+
+        self.load_data()
+
+        # Сигнал для обработки изменения ячеек
+        self.marksTable.itemChanged.connect(self.on_item_changed)
+
+        # Хранить измененные данные
+        self.modified_data = {}
+
+    def on_back_button_clicked(self):
+        from Presentation.Admin.adminWindow import AdminWindow
+        self.adminWindow = AdminWindow()
+        self.adminWindow.show()
+        self.close()
+
+    def load_data(self):
+        # Определите абсолютный путь к базе данных
+        db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Data/Recycling/RecyclingInfo.db'))
+        connection = sqlite3.connect(db_path)
+        query = "SELECT * FROM RecyclingInfo"
+        cursor = connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        column_names = [description[0] for description in cursor.description]
+
+        self.marksTable.setRowCount(len(data))
+        self.marksTable.setColumnCount(len(column_names))
+        self.marksTable.setHorizontalHeaderLabels(column_names)
+
+        for row_idx, row_data in enumerate(data):
+            for col_idx, col_data in enumerate(row_data):
+                self.marksTable.setItem(row_idx, col_idx, QtWidgets.QTableWidgetItem(str(col_data)))
+
+        connection.close()
+
+    def on_item_changed(self, item):
+        # Получить измененные данные
+        row = item.row()
+        column = item.column()
+        value = item.text()
+        column_name = self.marksTable.horizontalHeaderItem(column).text()
+
+        # Сохранить измененные данные
+        if row not in self.modified_data:
+            self.modified_data[row] = {}
+        self.modified_data[row][column_name] = value
+
+        # Обновить базу данных
+        self.update_database(row, column_name, value)
+
+    def update_database(self, row, column_name, value):
+        db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Data/Recycling/RecyclingInfo.db'))
+        connection = sqlite3.connect(db_path)
+        cursor = connection.cursor()
+
+        # Получить id записи для обновления
+        record_id = self.marksTable.item(row, 0).text()  # Предполагается, что первый столбец - id
+
+        query = f"UPDATE RecyclingInfo SET {column_name} = ? WHERE id = ?"
+        cursor.execute(query, (value, record_id))
+        connection.commit()
+        connection.close()
--- app/Presentation/Admin/adminWindow.py
+++ app/Presentation/Admin/adminWindow.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QGuiApplication
 from Presentation.UI.Admin.UiAdminMainWindow import Ui_AdminWindow
 from Presentation.Admin.adminRequestsWindow import AdminRequestsWindow
+from Presentation.Admin.adminRecycleInfoWindow import AdminRecycleInfoWindow
 
 class AdminWindow(QMainWindow, Ui_AdminWindow):
     def __init__(self):
@@ -11,6 +12,7 @@ class AdminWindow(QMainWindow, Ui_AdminWindow):
 
         self.move_center()
         self.requestsButton.clicked.connect(self.open_requests_window)
+        self.recyclingButton.clicked.connect(self.open_recycleInfo_window)
 
     def move_center(self):
         primary_screen = QGuiApplication.primaryScreen()
@@ -23,4 +25,9 @@ class AdminWindow(QMainWindow, Ui_AdminWindow):
     def open_requests_window(self):
         self.requests_window = AdminRequestsWindow()
         self.requests_window.show()
+        self.close()
+
+    def open_recycleInfo_window(self):
+        self.recycleInfo_window = AdminRecycleInfoWindow()
+        self.recycleInfo_window.show()
         self.close()
